b62af41133ae83825333e10a1c72c07005310de6
szbmarton <szbmarton@gmail.com>
2020-12-27 23:12:35 +0100

    fix-python-indents

--- python/postgres.py
+++ python/postgres.py
@@ -10,25 +10,25 @@ def connect():
         # connect to the PostgreSQL server
         print('Connecting to the PostgreSQL database...')
         conn = psycopg2.connect(host=os.environ.get('HOST'),
-								database=os.environ.get('DB'),
-								user=os.environ.get('USER'),
-								password=os.environ.get('PASSWD'))
-								
+                                database=os.environ.get('DB'),
+                                user=os.environ.get('USER'),
+                                password=os.environ.get('PASSWD'))
+                                
         # create a cursor
         cur = conn.cursor()
         
-	   # execute a statement
+       # execute a statement
         print('PostgreSQL database version:')
         cur.execute('SELECT version()')
 
         # display the PostgreSQL database server version
         db_version = cur.fetchone()
         print(db_version)
-		
-		# display the connection details
-		print(conn.dsn)
+        
+        # display the connection details
+        print(conn.dsn)
        
-	    # close the communication with the PostgreSQL
+        # close the communication with the PostgreSQL
         cur.close()
     except (Exception, psycopg2.DatabaseError) as error:
         print(error)
